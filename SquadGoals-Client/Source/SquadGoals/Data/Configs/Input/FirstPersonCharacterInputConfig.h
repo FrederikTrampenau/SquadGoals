50f2a5766d09b5d0002fcf90024fbd6af5e873a5
Frederik Trampenau <frederiktramp@gmail.com>
2024-11-13 18:20:30 +0100

    add: Create UFirstPersonCharacterInputConfig data asset
    - Create UFirstPersonCharacterInputConfig data asset which gets used to
    configure the input actions for a first person character

---
+++ SquadGoals-Client/Source/SquadGoals/Data/Configs/Input/FirstPersonCharacterInputConfig.h
@@ -0,0 +1,36 @@
+﻿// Copyright Frederik Trampenau All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "FirstPersonCharacterInputConfig.generated.h"
+
+class UInputAction;
+/**
+ * Class for configuring input actions for a first person character.
+ */
+UCLASS(Blueprintable)
+class SQUADGOALS_API UFirstPersonCharacterInputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UInputAction> MoveVertical;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UInputAction> MoveHorizontal;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UInputAction> LookVertical;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UInputAction> LookHorizontal;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UInputAction> Jump;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UInputAction> Shoot;
+};
