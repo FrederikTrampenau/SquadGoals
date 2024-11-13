f710ca2efcc8d532163a735eb5c5a303ada656d8
Frederik Trampenau <frederiktramp@gmail.com>
2024-11-13 18:03:35 +0100

    add: Create FFirstPersonInputData struct
    - Add a FFirstPersonInputData struct which will hold the current first
    person input data to process

---
+++ SquadGoals-Client/Source/SquadGoals/Data/Input/FFirstPersonInputData.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "FFirstPersonInputData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FFirstPersonInputData
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float HorizontalMoveInput;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float VerticalMoveInput;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float HorizontalLookInput;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float VerticalLookInput;
+};
