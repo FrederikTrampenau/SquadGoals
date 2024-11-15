7c90a429a8f48917e2dc3e46466dc0bb47ad894b
Frederik Trampenau <frederiktramp@gmail.com>
2024-11-15 21:34:30 +0100

    add: Add DebugPrintHelpers utility code
    - Add a DebugPrintHelpers file that can print debug messages with or
    without values

---
+++ SquadGoals-Client/Source/SquadGoals/Debug/DebugPrintHelpers.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#define PRINT_TEXT(Log, Value, Color) \
+if(GEngine) \
+{ \
+GEngine->AddOnScreenDebugMessage(-1, 0.2f, Color, FString::Printf(TEXT(Log), Value)); \
+}
+
+#define PRINT_TEXT(Log, Color) \
+if(GEngine) \
+{ \
+GEngine->AddOnScreenDebugMessage(-1, 0.2f, Color, FString::Printf(TEXT(Log))); \
+}
