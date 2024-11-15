5c76e34d17d92707dc5a7ff4118ed40c4ef4fba9
Frederik Trampenau <frederiktramp@gmail.com>
2024-11-15 22:05:02 +0100

    feat: Implement basic move and look functionality
    - Hold a reference to the input mapping and input actions
    - Listen to the start and end of move and look inputs as well as to the
    start of jump input
    - Store movement and look input and reset it when input stops
    - Apply move and look input every frame
    - Perform a jump when receiving jump input

--- SquadGoals-Client/Source/SquadGoals/Characters/ShootingRangeCharacter.cpp
+++ SquadGoals-Client/Source/SquadGoals/Characters/ShootingRangeCharacter.cpp
@@ -1,32 +1,127 @@
 ﻿// Copyright Frederik Trampenau All Rights Reserved.
 
-
 #include "ShootingRangeCharacter.h"
+#include "Components/CapsuleComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "SquadGoals/Data/Configs/Input/FirstPersonCharacterInputConfig.h"
+#include "SquadGoals/Debug/DebugPrintHelpers.h"
 
-
-// Sets default values
 AShootingRangeCharacter::AShootingRangeCharacter()
 {
-	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	CameraComponent->SetupAttachment(GetCapsuleComponent());
 }
 
-// Called when the game starts or when spawned
 void AShootingRangeCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
-// Called every frame
 void AShootingRangeCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	MoveWithInput();
 }
 
-// Called to bind functionality to input
 void AShootingRangeCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	const APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	UEnhancedInputLocalPlayerSubsystem* InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(
+		PlayerController->GetLocalPlayer());
+	InputSubsystem->ClearAllMappings();
+	InputSubsystem->AddMappingContext(InputMapping, 0);
+
+	if (!InputActions)
+	{
+		return;
+	}
+	
+	UEnhancedInputComponent* Input = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	Input->BindAction(InputActions->MoveVertical, ETriggerEvent::Triggered, this, &AShootingRangeCharacter::ProcessMoveVerticalInput);
+	Input->BindAction(InputActions->MoveVertical, ETriggerEvent::Completed, this, &AShootingRangeCharacter::ResetMoveVerticalInput);
+	Input->BindAction(InputActions->MoveHorizontal, ETriggerEvent::Triggered, this, &AShootingRangeCharacter::ProcessMoveHorizontalInput);
+	Input->BindAction(InputActions->MoveHorizontal, ETriggerEvent::Completed, this, &AShootingRangeCharacter::ResetMoveHorizontalInput);
+	Input->BindAction(InputActions->LookVertical, ETriggerEvent::Triggered, this, &AShootingRangeCharacter::ProcessLookVerticalInput);
+	Input->BindAction(InputActions->LookVertical, ETriggerEvent::Completed, this, &AShootingRangeCharacter::ResetLookVerticalInput);
+	Input->BindAction(InputActions->LookHorizontal, ETriggerEvent::Triggered, this, &AShootingRangeCharacter::ProcessLookHorizontalInput);
+	Input->BindAction(InputActions->LookHorizontal, ETriggerEvent::Completed, this, &AShootingRangeCharacter::ResetLookHorizontalInput);
+	Input->BindAction(InputActions->Jump, ETriggerEvent::Triggered, this, &AShootingRangeCharacter::ProcessJumpInput);
+	Input->BindAction(InputActions->Shoot, ETriggerEvent::Triggered, this, &AShootingRangeCharacter::ProcessShootInput);
 }
 
+void AShootingRangeCharacter::MoveWithInput()
+{
+	FVector2D MoveInputToProcess = { InputData.HorizontalMoveInput, InputData.VerticalMoveInput };
+	MoveInputToProcess.Normalize();
+
+	if (MoveInputToProcess.X != 0.0f)
+	{
+		const FVector Right = GetActorRightVector();
+		AddMovementInput(Right, MoveInputToProcess.X);
+	}
+	if (MoveInputToProcess.Y != 0.0f)
+	{
+		const FVector Forward = GetActorForwardVector();
+		AddMovementInput(Forward, MoveInputToProcess.Y);
+	}
+	
+	if (InputData.HorizontalLookInput != 0.0f)
+	{
+		AddControllerYawInput(InputData.HorizontalLookInput);
+	}
+	if (InputData.VerticalLookInput != 0.0f)
+	{
+		AddControllerPitchInput(InputData.VerticalLookInput);
+	}
+}
+
+void AShootingRangeCharacter::ProcessMoveVerticalInput(const FInputActionValue& InputValue)
+{
+	InputData.VerticalMoveInput = InputValue.Get<float>();
+}
+
+void AShootingRangeCharacter::ResetMoveVerticalInput(const FInputActionValue& InputValue)
+{
+	InputData.VerticalMoveInput = 0.0f;
+}
+
+void AShootingRangeCharacter::ProcessMoveHorizontalInput(const FInputActionValue& InputValue)
+{
+	InputData.HorizontalMoveInput = InputValue.Get<float>();
+}
+
+void AShootingRangeCharacter::ResetMoveHorizontalInput(const FInputActionValue& InputValue)
+{
+	InputData.HorizontalMoveInput = 0.0f;
+}
+
+void AShootingRangeCharacter::ProcessLookVerticalInput(const FInputActionValue& InputValue)
+{
+	InputData.VerticalLookInput = InputValue.Get<float>();
+}
+
+void AShootingRangeCharacter::ResetLookVerticalInput(const FInputActionValue& InputValue)
+{
+	InputData.VerticalLookInput = 0.0f;
+}
+
+void AShootingRangeCharacter::ProcessLookHorizontalInput(const FInputActionValue& InputValue)
+{
+	InputData.HorizontalLookInput = InputValue.Get<float>();
+}
+
+void AShootingRangeCharacter::ResetLookHorizontalInput(const FInputActionValue& InputValue)
+{
+	InputData.HorizontalLookInput = 0.0f;
+}
+
+void AShootingRangeCharacter::ProcessJumpInput(const FInputActionValue& InputValue)
+{
+	Jump();
+}
